cd8539fa1127cc98eac8e66f003c92da6deec24c
Rebecca Shoptaw <rebecca.shoptaw@gmail.com>
2024-02-01 13:24:01 -0500

    Begin app set-up.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.whiteText}>Ask Oscar Wilde</Text>
+      <Text style={[styles.whiteText, styles.smaller]}>your questions</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  whiteText: {
+    color: "#fff",
+  },
+  smaller: {
+    fontSize: 10,
   },
 });
